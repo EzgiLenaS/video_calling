0be247417c40f3bd89022b0489d5a6307e2d7ec2
EzgiLenaS <ezgilena00@gmail.com>
2025-08-21 04:35:54 +0300

    Sending Friend Request is done with a new model called FriendRequest to check pending and accepting. Also checking if the users already friends, the user send the friend to her/himself, or one of them already sent the request. Next is Accepting friend request endpoint.

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers, getMyFriends } from "../controllers/user.controller.js";
+import { getRecommendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest } from "../controllers/user.controller.js";
 
 const userRoutes = express.Router();
 
@@ -9,5 +9,8 @@ userRoutes.use(protectRoute);
 
 userRoutes.get("/", getRecommendedUsers);
 userRoutes.get("/friends", getMyFriends);
+
+userRoutes.post("/friend-request/:id", sendFriendRequest);
+userRoutes.put("/friend-request/:id/accept", acceptFriendRequest);
 
 export default userRoutes;
