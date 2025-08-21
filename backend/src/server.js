cf251d0bf66c9d8b7d3701cbba6d1642b335ef0e
EzgiLenaS <ezgilena00@gmail.com>
2025-08-22 02:48:36 +0300

    Cors errors are prevented and connected frontend to backend. Next is Signup Page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
@@ -24,7 +26,12 @@ app.get("/api/auth/logout", (req, res) => {
     res.send("Logout Route");
 }); */
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true // Allow frontend to send cookies
+}));
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
