23152a4d5cb46c5118e03145bb79592b0da715cf
EzgiLenaS <ezgilena00@gmail.com>
2025-08-27 23:25:50 +0300

    Home Page is done and Layout, sidebar are added to the other pages too.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,72 @@
-import React from 'react'
 import { Navigate, Route, Routes } from 'react-router'
+
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
-import PageLoader from './components/PageLoader.jsx';
+
+
+import Layout from "./components/Layout.jsx";
+import { useThemeStore } from "./store/useThemeStore.js";
 import useAuthUser from './hooks/useAuthUser.js';
+import PageLoader from './components/PageLoader.jsx';
+
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
 
   const isAuthenticated = Boolean(authUser);
+  const { theme } = useThemeStore();
 
   // Where does the authData come from???
   // const authUser = authData?.userWhichUsedInAppAndAuthRoute;
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className='h-screen' data-theme="aqua">
+    <div className="h-screen" data-theme={theme}>
       <Routes>
-        <Route path="/" element={ isAuthenticated ? <HomePage /> : <Navigate to="/login" /> } />
-        <Route path="/signup" element={ !isAuthenticated ? <SignUpPage /> : <Navigate to="/" /> } />
-        <Route path="/login" element={ !isAuthenticated ? <LoginPage /> : <Navigate to="/" /> } />
-        <Route path="/notifications" element={ authUser ? <NotificationsPage /> : <Navigate to="/login" /> } />
-        <Route path="/call" element={ isAuthenticated ? <CallPage /> : <Navigate to="/login" /> } />
-        <Route path="/chat" element={ isAuthenticated ? <ChatPage /> : <Navigate to="/login" /> } />
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Layout showSidebar={true}>
+                <HomePage />
+              </Layout>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route path="/signup" element={ !isAuthenticated ? (<SignUpPage />) : (<Navigate to="/" />) } />
+        <Route path="/login" element={ !isAuthenticated ? (<LoginPage />) : (<Navigate to="/" />) } />
+        <Route
+          path="/notifications"
+          element={
+            authUser ? (
+              <Layout showSidebar={true}>
+                <NotificationsPage />
+              </Layout>
+              ) : (
+              <Navigate to="/login" />
+              ) 
+            }
+        />
+        <Route path="/call" element={ isAuthenticated ? (<CallPage />) : (<Navigate to="/login" />) } />
+        <Route
+          path="/chat"
+          element={
+            isAuthenticated ? (
+              <Layout showSidebar={false}>
+                <ChatPage />
+              </Layout>
+              ) : (
+              <Navigate to="/login" />
+              )
+            }
+        />
       </Routes>
 
       <Toaster />
